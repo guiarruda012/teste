9cfe8305c7e3010017290c9fa1290797d7c2cb3f
Guilherme <guiarruda2601@gmail.com>
2025-08-19 10:42:02 -0300

    Sem bom dia!

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ function calculoSala (largura, altura) {                               //       
     let numero = 9;
     calcularTabuada(numero); */
 
-    prompt('Bom dia');
+    prompt('Sem bom dia!');
 
 
     let listaDeProgramacao = ['python', 'javaScript', 'java', 'c#'];
